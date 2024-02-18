d28bf1bad3cf1c83f7fa0da8350fc20cd7643c69
dev-gsilv <dev.gsilv@gmail.com>
2024-02-18 13:51:30 -0300

    feat: complete user CRUD, no validations

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,40 @@
 import { User } from '../models/user.model.js';
 
 export async function findAll(req, res) {
-    User.findAll().then((result) => {res.json(result)})
+    User.findAll().then(result => {
+        res.json(result);
+    });
+}
+
+export async function findById(req, res) {
+    const id = req.params.id;
+    User.findByPk(id).then(result => {
+        res.json(result);
+    });
+}
+
+export async function create(req, res) {
+    const data = req.body;
+    User.create(data).then(result => {
+        res.json(result);
+    });
+}
+
+export async function update(req, res) {
+    const data = req.body;
+    const id = req.params.id;
+
+    User.update(data, {
+        where: { id: id },
+    }).then(result => {
+        res.json(result);
+    });
+}
+
+export async function remove(req, res) {
+    const id = req.params.id;
+
+    User.destroy({ where: { id: id } }).then(result => {
+        res.json(result);
+    });
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { testing } from '../controllers/health.controller.js';
-import { findAll } from '../controllers/user.controller.js';
+import { findAll, findById, create, update, remove } from '../controllers/user.controller.js';
 
 export const routes = express.Router();
 
 routes.get('/healthcheck', testing);
 routes.get('/users', findAll);
+routes.get('/users/:id', findById);
+routes.post('/users', create);
+routes.put('/users/:id', update);
+routes.delete('/users/:id', remove);
